b793a8662041a97ece11d60e8ba4f189f49691a9
John <juanluiscc@gmail.com>
2025-03-08 18:53:08 +0100

    Update checkURL2.py

--- app/python/checkURL2.py
+++ app/python/checkURL2.py
@@ -4,41 +4,16 @@ import smtplib
 from email.mime.text import MIMEText
 from email.mime.multipart import MIMEMultipart
 
-# Configuración del correo
-def enviar_email(remitente, destinatario, asunto, mensaje):
-    try:
-        # Servidor SMTP de Gmail
-        servidor_smtp = "in-v3.mailjet.com"
-        puerto_smtp = 587
+# Argumentos de este script : URL + APIKEY + SECRETKEY
+# APIKEY y SECRETKEY del smtp server
 
-        # Crear el mensaje
-        msg = MIMEMultipart()
-        msg["From"] = remitente
-        msg["To"] = destinatario
-        msg["Subject"] = asunto
-
-        # Agregar cuerpo del mensaje
-        cuerpo = MIMEText(mensaje, "plain")
-        msg.attach(cuerpo)
-
-        # Conectar al servidor SMTP
-        with smtplib.SMTP(servidor_smtp, puerto_smtp) as servidor:
-            servidor.starttls()  # Iniciar la conexión segura
-            servidor.login(remitente, contraseña)  # Iniciar sesión
-            servidor.send_message(msg)  # Enviar mensaje
-
-        print("Correo enviado correctamente.")
-
-    except Exception as e:
-        print(f"Error al enviar el correo: {e}")
-
-
-
-
-# Verificar si se pasó el parámetro
-if len(sys.argv) > 1:
+# Verificar si se pasaron los parametros
+if len(sys.argv) > 3:
     url = sys.argv[1]
     print(f"URL to be checked: {url}")
+
+    APIKEY = sys.argv[2]
+    SECRETKEY = sys.argv[3]
 
     # Haciendo la solicitud GET
     response = urllib.request.urlopen(url)
@@ -58,17 +33,46 @@ if len(sys.argv) > 1:
     else:
          print(f"URL DOWN  ( {status} )"
         # Datos para enviar el correo
-        remitente = "juanluiscc@gmail.com"
         destinatario = "juanluiscc@gmail.com"
         asunto = "URL Down"
-        mensaje = "URL DOWN  ( {status} )"
+        mensaje = "URL DOWN  ( URL :{url}   HTTP Status :{status} )"
         
         
 
         # Llamar a la función para enviar el correo
-        enviar_email(remitente, destinatario, asunto, mensaje, contraseña)
+        enviar_email(destinatario, asunto, mensaje, APIKEY, SECRETKEY)
 
 else:
     print("Not received url.")
+
+
+
+# Configuración del correo
+def enviar_email(destinatario, asunto, mensaje, APIKEY, SECRETKEY):
+    try:
+        # Servidor SMTP de Gmail
+        servidor_smtp = "in-v3.mailjet.com"
+        puerto_smtp = 587
+
+        # Crear el mensaje
+        msg = MIMEMultipart()
+        msg["From"] = remitente
+        msg["To"] = destinatario
+        msg["Subject"] = asunto
+
+        # Agregar cuerpo del mensaje
+        cuerpo = MIMEText(mensaje, "plain")
+        msg.attach(cuerpo)
+
+        # Conectar al servidor SMTP
+        with smtplib.SMTP(servidor_smtp, puerto_smtp) as servidor:
+            servidor.starttls()  # Iniciar la conexión segura
+            servidor.login(APIKEY, SECRETKEY)  # Iniciar sesión
+            servidor.send_message(msg)  # Enviar mensaje
+
+        print("Correo enviado correctamente.")
+
+    except Exception as e:
+        print(f"Error al enviar el correo: {e}")
 
 
