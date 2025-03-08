28f2baf2491e4b9952497d8fbdfb51d138c714de
John <juanluiscc@gmail.com>
2025-03-08 18:23:00 +0100

    Update checkURL2.py

--- app/python/checkURL2.py
+++ app/python/checkURL2.py
@@ -1,5 +1,39 @@
 import urllib.request
 import sys
+import smtplib
+from email.mime.text import MIMEText
+from email.mime.multipart import MIMEMultipart
+
+# Configuración del correo
+def enviar_email(remitente, destinatario, asunto, mensaje):
+    try:
+        # Servidor SMTP de Gmail
+        servidor_smtp = "in-v3.mailjet.com"
+        puerto_smtp = 587
+
+        # Crear el mensaje
+        msg = MIMEMultipart()
+        msg["From"] = remitente
+        msg["To"] = destinatario
+        msg["Subject"] = asunto
+
+        # Agregar cuerpo del mensaje
+        cuerpo = MIMEText(mensaje, "plain")
+        msg.attach(cuerpo)
+
+        # Conectar al servidor SMTP
+        with smtplib.SMTP(servidor_smtp, puerto_smtp) as servidor:
+            servidor.starttls()  # Iniciar la conexión segura
+            servidor.login(remitente, contraseña)  # Iniciar sesión
+            servidor.send_message(msg)  # Enviar mensaje
+
+        print("Correo enviado correctamente.")
+
+    except Exception as e:
+        print(f"Error al enviar el correo: {e}")
+
+
+
 
 # Verificar si se pasó el parámetro
 if len(sys.argv) > 1:
@@ -15,8 +49,24 @@ if len(sys.argv) > 1:
     # Mostrando el contenido
     print(html)
 
+    status = response.getcode()
     # Si quieres obtener solo el código de estado HTTP
-    print(f"HTTP status code: {response.getcode()}")
+    print(f"HTTP status code: {status}")
+
+    if status == 200:
+        print(f"URL UP")
+    else:
+         print(f"URL DOWN  ( {status} )"
+        # Datos para enviar el correo
+        remitente = "juanluiscc@gmail.com"
+        destinatario = "juanluiscc@gmail.com"
+        asunto = "URL Down"
+        mensaje = "URL DOWN  ( {status} )"
+        contraseña = "50b7e929159b0ac52e58e0d63ad3c17a"
+        
+
+        # Llamar a la función para enviar el correo
+        enviar_email(remitente, destinatario, asunto, mensaje, contraseña)
 
 else:
     print("Not received url.")
